ea5864311a967ccbbd8da41e1777e520e1245d0a
Jahidul Islam <97932459+jahidul96@users.noreply.github.com>
2023-02-21 14:17:29 +0600

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -5,12 +5,7 @@ import { initializeFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAKJQjrXitEk4itJIbqTvpf3nKmATS5FHw",
-  authDomain: "blog-rn12.firebaseapp.com",
-  projectId: "blog-rn12",
-  storageBucket: "blog-rn12.appspot.com",
-  messagingSenderId: "994400930478",
-  appId: "1:994400930478:web:b2e3f72090ff758351fb3f",
+//   your key
 };
 
 const app = initializeApp(firebaseConfig);
